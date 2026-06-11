490a6f7074eb16f27902e432e867f52e2f03ab1d
POrGSrPOGS <POrGSrPOGS@gmail.com>
2026-06-11 21:32:21 +0100

    Updated question answers and fixed colour coding of mark feedback

--- client/src/components/Marks.jsx
+++ client/src/components/Marks.jsx
@@ -11,21 +11,23 @@ export default function Marks({ marks }) {
 
     if (mark === maxMark) {
         feedback = "Perfection!"
-        colour = "green-400"
+        colour = "text-green-400"
     } else if (mark > 0) {
         feedback = "Almost!"
-        colour = "orange-400"
+        colour = "text-orange-400"
     } else {
         feedback = "Better luck next time"
-        colour = "red-400"
+        colour = "text-red-400"
     }
+
+    console.log(`text-3xl mt-5 font-bold text-${colour}`)
     
     return(
 
-        <div className = {`text-3xl mt-5 font-bold text-${colour}`}>
+        <div className = {`text-3xl mt-5 font-bold ${colour}`}>
             {`[ ${mark} / ${maxMark} ] ${feedback}`}
             
-            <ul className= "text-xl mt-10 font-mono text-green-600">
+            <ul className= "text-xl mt-10 font-mono text-green">
                 <div className = "mb-4"> Correct Answers: </div>
                 {correctAnswers.map((correctAnswer, index) => (
                     <li key={index}>{`• "${correctAnswer}" [1] `}</li>
